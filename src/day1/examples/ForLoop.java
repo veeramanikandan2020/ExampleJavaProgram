52b45172dc871e8257044e0399b5deeeadfe0fdf
veera <veeramanikandan2020@gmail.com>
2023-08-29 19:50:48 +0530

    Print i and a value

--- src/day1/examples/ForLoop.java
+++ src/day1/examples/ForLoop.java
@@ -8,7 +8,9 @@ public class ForLoop {
 		int j = 0;
 		for(i=0;i<a;i++)
 		{
-			//for(j=a;j>i;j--) 
+			//for(j=a;j>i;j--)
+			//Test of git - conflict issue added below print statement
+			System.out.println(i+" "+a);
 			System.out.println("*");
 		   
 	    }
